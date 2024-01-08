6c378a8b3ac0ca273b6e24dd6785b9fd2c14e684
Dimas Wisnu Wirawan <dimasw347@gmail.com>
2024-01-08 09:28:48 +0700

    update pagination pada permintaan dan stok

--- resources/views/livewire/permintaan/proses.blade.php
+++ resources/views/livewire/permintaan/proses.blade.php
@@ -247,7 +247,7 @@
                 <div class="flex ">
                     <div class="flex space-x-4 items-center mb-3">
                         <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
-                        <select wire:model.live="perpage"
+                        <select wire:model.live="perPage"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                             <option value="5">5</option>
                             <option value="10">10</option>
